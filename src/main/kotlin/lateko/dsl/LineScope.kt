b8478c9de6265eb890073ae929f0ef3be89797d2
Durun <negiigainuki@gmail.com>
2020-03-06 19:43:09 +0900

    refactor

--- src/main/kotlin/lateko/dsl/LineScope.kt
+++ src/main/kotlin/lateko/dsl/LineScope.kt
@@ -7,6 +7,14 @@ import lateko.element.LineComposition
 import lateko.element.LineElement
 
 open class LineScope : Builder<LineElement, LineComposition>() {
+	companion object {
+		fun (LineScope.() -> Unit).build(): LineComposition {
+			val builder = LineScope()
+			builder.this()
+			return builder.build()
+		}
+	}
+
 	operator fun InlineElement.unaryMinus() {
 		elements += Line(this)
 	}
--- src/main/kotlin/lateko/dsl/structure/StructureScope.kt
+++ src/main/kotlin/lateko/dsl/structure/StructureScope.kt
@@ -3,41 +3,36 @@ package lateko.dsl.structure
 import lateko.command.TexCommand
 import lateko.dsl.Builder
 import lateko.dsl.LineScope
+import lateko.dsl.LineScope.Companion.build
 import lateko.dsl.inline.text
 import lateko.element.*
 
 open class StructureScope : Builder<StructureElement, StructureComposition>() {
-	val contents: List<StructureElement> get() = elements
+	companion object {
+		private fun StructureComposition.sortNest(): StructureComposition {
+			return this.copy(children = children.sortedBy { it is Section || it is Chapter })
+		}
 
-	private fun StructureComposition.sortNest(): StructureComposition {
-		return this.copy(children = children.sortedBy { it is Section || it is Chapter })
+		fun (StructureScope.() -> Unit).build(): StructureComposition {
+			val builder = StructureScope()
+			builder.this()
+			return builder.build().sortNest()
+		}
 	}
 
 	private fun TexCommand.toLine(): Structure = Structure(Line(EmbeddedCode(this.toString(), format = EmbeddedCode.Format.Tex)))
 
-	protected fun <T : StructureElement> addStructure(content: StructureScope.() -> Unit, constructor: (StructureElement) -> T): T {
-		val builder = StructureScope()
-		builder.content()
-		val element = constructor(builder.build().sortNest())
-		elements += element
-		return element
-	}
-
 	fun makeTitle(): Structure = TexCommand("maketitle").toLine().adding()
 
-	fun section(name: InlineElement? = null, content: StructureScope.() -> Unit): Section = addStructure(content) { Section(name = name, content = it) }
+	fun section(name: InlineElement? = null, content: StructureScope.() -> Unit): Section = Section(name = name, content = content.build()).adding()
 
 	fun section(name: String, content: StructureScope.() -> Unit) = section(name.text, content)
 
-	fun chapter(name: InlineElement, content: StructureScope.() -> Unit): Chapter = addStructure(content) { Chapter(name = name, content = it) }
+	fun chapter(name: InlineElement, content: StructureScope.() -> Unit): Chapter = Chapter(name = name, content = content.build()).adding()
 
 	fun chapter(name: String, content: StructureScope.() -> Unit) = chapter(name.text, content)
 
-	fun p(content: LineScope.() -> Unit): Paragraph {
-		val paragraph = Paragraph.of(content)
-		elements += paragraph
-		return paragraph
-	}
+	fun p(content: LineScope.() -> Unit): Paragraph = Paragraph(content.build()).adding()
 
 	override fun build(): StructureComposition = StructureComposition(elements)
 }
