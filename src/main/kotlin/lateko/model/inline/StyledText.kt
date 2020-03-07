af55017a9f83b624c5ff0d6dd09e74afeffd5518
Durun <negiigainuki@gmail.com>
2020-03-07 19:52:33 +0900

    develop for #6

--- src/main/kotlin/lateko/model/inline/StyledText.kt
+++ src/main/kotlin/lateko/model/inline/StyledText.kt
@@ -2,6 +2,7 @@ package lateko.model.inline
 
 data class StyledText(val text: Text, val styles: Set<Style>) : InlineElement {
 	override fun <R> accept(visitor: InlineVisitor<R>): R = visitor.visit(this)
+	val string: String = text.text
 
 	enum class Style {
 		Strong,
