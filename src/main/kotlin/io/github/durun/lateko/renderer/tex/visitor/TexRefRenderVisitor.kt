962248cad41571a0ec459a8b4235f901fb788ce3
Durun <negiigainuki@gmail.com>
2020-03-24 04:17:27 +0900

    fix

--- src/main/kotlin/io/github/durun/lateko/renderer/tex/visitor/TexRefRenderVisitor.kt
+++ src/main/kotlin/io/github/durun/lateko/renderer/tex/visitor/TexRefRenderVisitor.kt
@@ -1,11 +1,11 @@
 package io.github.durun.lateko.renderer.tex.visitor
 
-import io.github.durun.lateko.command.tex.Ref
+import io.github.durun.lateko.model.inline.EmbeddedCode
 import io.github.durun.lateko.model.inline.Reference
 import io.github.durun.lateko.renderer.common.InlineRenderVisitor
 
 internal interface TexRefRenderVisitor : InlineRenderVisitor {
 	override fun visit(ref: Reference): String {
-		return Ref(ref.id).toString()
+		return ref.renderedAs(EmbeddedCode.Format.Tex).orEmpty()
 	}
 }
