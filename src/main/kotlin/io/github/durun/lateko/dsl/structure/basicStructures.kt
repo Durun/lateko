5fd4c487ab30fea14bd06734a08f3c6d10e9c504
Durun <negiigainuki@gmail.com>
2020-04-10 18:18:03 +0900

    format

--- src/main/kotlin/io/github/durun/lateko/dsl/structure/basicStructures.kt
+++ src/main/kotlin/io/github/durun/lateko/dsl/structure/basicStructures.kt
@@ -8,14 +8,45 @@ import io.github.durun.lateko.model.line.Line.Companion.toLine
 import io.github.durun.lateko.model.structure.Chapter
 import io.github.durun.lateko.model.structure.Section
 
-fun DocumentScope.chapter(title: InlineElement, label: String? = null, content: ChapterScope.() -> Unit): Chapter = io.github.durun.lateko.dsl.chapter(title, label = label, content = content).adding()
-fun DocumentScope.chapter(title: String, label: String? = null, content: ChapterScope.() -> Unit): Chapter = io.github.durun.lateko.dsl.chapter(title, label = label, content = content).adding()
 
-fun ChapterScope.section(name: InlineElement? = null, label: String? = null, content: SectionScope.() -> Unit): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
-fun ChapterScope.section(name: String, label: String? = null, content: SectionScope.() -> Unit): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+fun DocumentScope.chapter(
+		title: InlineElement,
+		label: String? = null,
+		content: ChapterScope.() -> Unit
+): Chapter = io.github.durun.lateko.dsl.chapter(title, label = label, content = content).adding()
 
-fun SectionScope.section(name: InlineElement? = null, label: String? = null, content: SectionScope.() -> Unit): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
-fun SectionScope.section(name: String, label: String? = null, content: SectionScope.() -> Unit): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+fun DocumentScope.chapter(
+		title: String,
+		label: String? = null,
+		content: ChapterScope.() -> Unit
+): Chapter = io.github.durun.lateko.dsl.chapter(title, label = label, content = content).adding()
+
+
+fun ChapterScope.section(
+		name: InlineElement? = null,
+		label: String? = null,
+		content: SectionScope.() -> Unit
+): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+
+fun ChapterScope.section(
+		name: String,
+		label: String? = null,
+		content: SectionScope.() -> Unit
+): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+
+
+fun SectionScope.section(
+		name: InlineElement? = null,
+		label: String? = null,
+		content: SectionScope.() -> Unit
+): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+
+fun SectionScope.section(
+		name: String,
+		label: String? = null,
+		content: SectionScope.() -> Unit
+): Section = io.github.durun.lateko.dsl.section(name, label = label, content = content).adding()
+
 
 fun StructureScope.paragraph(content: ParagraphScope.() -> Unit): Paragraph = io.github.durun.lateko.dsl.paragraph(content).adding()
 fun StructureScope.p(content: ParagraphScope.() -> Unit): Paragraph = paragraph(content)
