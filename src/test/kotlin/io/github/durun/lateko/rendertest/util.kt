c049d5564c4cfc957ba18c5a204fe9e61a9cac44
Durun <negiigainuki@gmail.com>
2020-04-10 11:49:44 +0900

    refactor test

--- src/test/kotlin/io/github/durun/lateko/rendertest/util.kt
+++ src/test/kotlin/io/github/durun/lateko/rendertest/util.kt
@@ -1,33 +1,27 @@
 package io.github.durun.lateko.rendertest
 
-import io.github.durun.lateko.model.Document
+import io.github.durun.lateko.model.structure.StructureElement
+import io.github.durun.lateko.parser.DocumentScriptParser
 import io.github.durun.lateko.renderer.markdown.BasicMarkdownRenderer
 import io.github.durun.lateko.renderer.markdown.MarkdownRenderer
 import io.github.durun.lateko.renderer.tex.BasicTexRenderer
 import io.github.durun.lateko.renderer.tex.TexRenderer
 import java.io.File
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
 
 
-internal val scriptEngine: ScriptEngine = ScriptEngineManager().getEngineByExtension("kts")!!
-
-internal fun generateDocument(script: File): Document {
+internal fun generateDocument(script: File): StructureElement {
 	val reader = script.reader()
 	println("Parsing: $script")
-	val obj = runCatching { scriptEngine.eval(reader) }
-			.getOrThrow()
-	return runCatching { obj as Document }
-			.getOrThrow()
+	return DocumentScriptParser().parse(script)
 }
 
-internal fun generateMarkdown(document: Document): String {
+internal fun generateMarkdown(document: StructureElement): String {
 	val renderer: MarkdownRenderer = BasicMarkdownRenderer
 	println("Generating Markdown...")
 	return renderer.render(document)
 }
 
-internal fun generateTex(document: Document): String {
+internal fun generateTex(document: StructureElement): String {
 	val renderer: TexRenderer = BasicTexRenderer
 	println("Generating TeX...")
 	return renderer.render(document)
