3a445a2c418223351a7e9ab01c43152a7c57f580
Durun <negiigainuki@gmail.com>
2020-04-10 22:45:29 +0900

    implement of #16

--- src/main/kotlin/io/github/durun/lateko/model/inline/Image.kt
+++ src/main/kotlin/io/github/durun/lateko/model/inline/Image.kt
@@ -1,22 +1,27 @@
 package io.github.durun.lateko.model.inline
 
+import io.github.durun.lateko.model.Labeled
+import io.github.durun.lateko.renderer.markdown.anchor
 import io.github.durun.lateko.target.tex.BeginEnd
 import io.github.durun.lateko.target.tex.Caption
+import io.github.durun.lateko.target.tex.Label
 import io.github.durun.lateko.target.tex.SimpleTexCommand
 
 class Image(
 		val title: String,
-		val path: String
-) : InlineExtension {
+		val path: String,
+		override val label: String = path
+) : InlineExtension, Labeled {
 	override fun renderPlane(): String = title
 	override fun renderTex(): String {
 		return BeginEnd("figure") {
 			"""
+				${Label(label)}
 				${SimpleTexCommand("includegraphics", listOf("width=\\linewidth"), arg = path)}
 				${Caption(title)}
 			""".trimIndent()
 		}.toString()
 	}
 
-	override fun renderMarkdown(): String = "![$title]($path)"
+	override fun renderMarkdown(): String = "![$title]($path)$anchor"
 }
