b77bdefc37be015b4f3de2a33540f9d194c0446b
Durun <negiigainuki@gmail.com>
2020-04-10 13:24:48 +0900

    refactor

--- src/main/kotlin/io/github/durun/lateko/model/structure/Structure.kt
+++ src/main/kotlin/io/github/durun/lateko/model/structure/Structure.kt
@@ -5,7 +5,6 @@ import io.github.durun.lateko.model.line.LineElement
 interface Structure : StructureElement {
 	val element: LineElement
 	override fun <R> accept(visitor: StructureVisitor<R>): R = visitor.visit(this)
-	override val context: StructureContext? get() = null
 
 	companion object {
 		fun LineElement.toStructure(): Structure = StructureData(this)
