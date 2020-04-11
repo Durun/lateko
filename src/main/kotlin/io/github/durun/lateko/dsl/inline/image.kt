647dedc09a79aca9335122c61e7f57c9996c961e
Durun <negiigainuki@gmail.com>
2020-04-11 09:30:55 +0900

    add image function

--- src/main/kotlin/io/github/durun/lateko/dsl/inline/image.kt
+++ src/main/kotlin/io/github/durun/lateko/dsl/inline/image.kt
@@ -3,3 +3,4 @@ package io.github.durun.lateko.dsl.inline
 import io.github.durun.lateko.model.inline.Image
 
 fun image(title: String, path: String): Image = Image(title = title, path = path)
+fun image(title: String, path: String, label: String): Image = Image(title = title, path = path, label = label)
